42043101b4d3f6a78968df819a5eaacb872b289b
sehdez <sergio.hdez.cuevas@gmail.com>
2022-05-07 19:42:16 -0500

    Escritorio update

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,41 @@
 
 
-console.log('Escritorio HTML');
+const searchParams = new URLSearchParams( window.location.search );
+if ( !searchParams.has('escritorio') ){
+    window.location = 'index.html';
+    throw new Error('El escritorio es obligatorio');
+}
+
+const escritorio = searchParams.get('escritorio');
+
+
+const socket = io();
+
+
+
+socket.on('connect', () => {
+    // console.log('Conectado');
+    // btnCrear.disabled = false;
+
+});
+
+socket.on('ultimo-ticket', (ultimoTicket) => {
+    // lblNuevoTicket.innerText = "Ticket " + ultimoTicket;
+})
+
+socket.on('disconnect', () => {
+    // console.log('Desconectado del servidor');
+    // btnCrear.disabled = true;
+
+});
+
+
+
+
+// btnCrear.addEventListener( 'click', () => {
+
+//     socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+//         // lblNuevoTicket.innerText = ticket;
+//     });
+
+// });
